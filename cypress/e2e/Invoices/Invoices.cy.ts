d8008a2b47046445f61a1736218928538517ec67
valentinogagliardi <valentino@valentinog.com>
2022-10-04 23:06:52 +0200

    Preparing skeleton test

---
+++ cypress/e2e/Invoices/Invoices.cy.ts
@@ -0,0 +1,14 @@
+context("Application", () => {
+  describe("Invoice", () => {
+    it("[Insert persona here] can create a new invoice", () => {
+      /**
+       * Your turn here ... we need to:
+       * 1. test all the form fields
+       * 2. intercept the XHR call
+       * 3. assert on the request body
+       * 4. test and write logic to enable the "Send email" button as soon as the first call is done
+       */
+      // Your assertions here
+    });
+  });
+});
