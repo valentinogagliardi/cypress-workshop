4d6fb0428b9ec4030a9c90fef487d94908e2de4b
valentinogagliardi <valentino@valentinog.com>
2022-10-06 00:00:27 +0200

    Rearrange stuff

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,11 +6,11 @@ export default function Index() {
       <h1>Welcome to the Cypress Workshop!</h1>
       <h2>Here are a series of exercises to be taken in order.</h2>
       <div className="wrapper">
-        <Link to="/users/create">
-          [01 - Fundamentals] Form testing, network stubbing
-        </Link>
         <Link to="/posts">[01 - Fundamentals] Network stubbing</Link>
-        <Link to="/users">[01 - Fundamentals] SSR Network stubbing</Link>
+        <Link to="/invoices/create">
+          [02 - Fundamentals] Form testing, network stubbing
+        </Link>
+        <Link to="/users">[03 - Fundamentals] SSR Network stubbing</Link>
       </div>
     </div>
   );
